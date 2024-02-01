d8a5a7eb02c9c38d93a43058c753671b2b34a8cd
Gera <gr.misterg@gmail.com>
2024-01-31 21:50:52 -0600

    Class save

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'; // this must be always imported 
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+const App = () =>{
+  return(
+    <View style = {style.container}>
+      <Text style = {style.txtHelloWorld}>Hello, world!</Text>
+      <Text style = {style.txtname}>I'm Ger</Text>
+      <Text style = {style.txtname}>I'm programming in class</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const style = StyleSheet.create({
+  container : {
+    width : '100%',
+    height : '100%',
+    backgroundColor: '#5352ed',
+    justifyContent : 'center',
+    alignItems : 'ceneter'
   },
+  txtHelloWorld : {
+    fontSize : 40,
+    fontFamily : 'Helvetica',
+    fontWeight : 'bold',
+    color : '#fff'
+  },
+  txtName : {
+    fontSize : 32,
+    fontFamily : 'Helvetica',
+    fontWeight : 'bold',
+    color : '#fff'
+  }
 });
+
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
